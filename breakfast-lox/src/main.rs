ecdf73ad2ccb8ee215031d2c8e431899eac08c9a
Kostya <happy.boat9793@fastmail.com>
2025-10-21 07:05:17 +0100

    wip

--- breakfast-lox/src/main.rs
+++ breakfast-lox/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read};
+use std::path::PathBuf;
+
+#[derive(Parser)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Run {
+        /// Script to execute
+        script: Option<PathBuf>,
+    },
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::try_parse()?;
+
+    let _script = match args.command {
+        Commands::Run { script } => {
+            let mut reader: Box<dyn BufRead> = match script {
+                None => Box::new(BufReader::new(std::io::stdin())),
+                Some(path) => Box::new(BufReader::new(File::open(path)?)),
+            };
+            let mut contents = String::new();
+            reader.read_to_string(&mut contents)?;
+            contents
+        }
+    };
+
+    Ok(())
 }
