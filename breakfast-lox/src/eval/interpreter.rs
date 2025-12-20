f2d751a42ada8ef6425a010c2ce425ed6a31cb64
Kostya <happy.boat9793@fastmail.com>
2025-12-20 16:21:28 +0000

    shorter

--- breakfast-lox/src/eval/interpreter.rs
+++ breakfast-lox/src/eval/interpreter.rs
@@ -5,101 +5,70 @@ use crate::ast::{AddOp, BinExpr, BinOp, CmpOp, EqOp, Expr, Lit, MulOp, RelOp, Un
 pub struct Interpreter;
 
 impl Interpreter {
-    fn eval_lit(&self, lit: &Lit) -> Result<Value, RuntimeError> {
-        Ok(match lit {
-            Lit::Nil(_) => Value::Nil,
-            Lit::Bool(x) => Value::Bool(x.0),
-            Lit::Num(x) => Value::Num(x.0),
-            Lit::Str(x) => Value::Str(x.0.clone()),
-        })
-    }
-
-    fn eval_un_expr(&self, expr: &UnExpr) -> Result<Value, RuntimeError> {
-        match expr {
-            UnExpr { op, e } => {
-                match (op, self.eval(&e)?) {
-                    (UnOp::Neg, Value::Nil) => Err(InvalidOperandTypeError::UnOpNegOnNil)?,
-                    (UnOp::Neg, Value::Bool(_)) => Err(InvalidOperandTypeError::UnOpNegOnBool)?,
-                    (UnOp::Neg, Value::Str(_)) => Err(InvalidOperandTypeError::UnOpNegOnStr)?,
-                    (UnOp::Not, Value::Num(_)) => Err(InvalidOperandTypeError::UnOpNotOnNum)?,
-                    (UnOp::Not, Value::Str(_)) => Err(InvalidOperandTypeError::UnOpNotOnStr)?,
-                    (UnOp::Neg, Value::Num(x)) => {
-                        // TODO(kostya): check if `-x` is representable
-                        Ok(Value::Num(-x))
-                    }
-                    (UnOp::Not, Value::Nil) => {
-                        // https://craftinginterpreters.com/evaluating-expressions.html#truthiness-and-falsiness
-                        Ok(Value::Bool(true))
-                    }
-                    (UnOp::Not, Value::Bool(x)) => Ok(Value::Bool(!x)),
-                }
+    fn eval_un_expr(&self, op: &UnOp, e: &Expr) -> Result<Value, RuntimeError> {
+        match (op, self.eval(&e)?) {
+            (UnOp::Neg, Value::Nil) => Err(InvalidOperandTypeError::UnOpNegOnNil)?,
+            (UnOp::Neg, Value::Bool(_)) => Err(InvalidOperandTypeError::UnOpNegOnBool)?,
+            (UnOp::Neg, Value::Str(_)) => Err(InvalidOperandTypeError::UnOpNegOnStr)?,
+            (UnOp::Not, Value::Num(_)) => Err(InvalidOperandTypeError::UnOpNotOnNum)?,
+            (UnOp::Not, Value::Str(_)) => Err(InvalidOperandTypeError::UnOpNotOnStr)?,
+            (UnOp::Neg, Value::Num(x)) => {
+                // TODO(kostya): check if `-x` is representable
+                Ok(Value::Num(-x))
             }
+            (UnOp::Not, Value::Nil) => {
+                // https://craftinginterpreters.com/evaluating-expressions.html#truthiness-and-falsiness
+                Ok(Value::Bool(true))
+            }
+            (UnOp::Not, Value::Bool(x)) => Ok(Value::Bool(!x)),
         }
     }
 
-    fn eval_bin_expr(&self, expr: &BinExpr) -> Result<Value, RuntimeError> {
-        match expr {
-            BinExpr { op, l, r } => match (op, self.eval(&l)?, self.eval(&r)?) {
-                (BinOp::Rel(RelOp::Eq(EqOp::Eq)), Value::Nil, Value::Nil) => Ok(Value::Bool(true)),
-                (BinOp::Rel(RelOp::Eq(EqOp::Eq)), Value::Nil, _)
-                | (BinOp::Rel(RelOp::Eq(EqOp::Eq)), _, Value::Nil) => Ok(Value::Bool(false)),
-                (BinOp::Rel(RelOp::Eq(EqOp::Eq)), Value::Bool(l), Value::Bool(r)) => {
-                    Ok(Value::Bool(l == r))
-                }
-                (BinOp::Rel(RelOp::Eq(EqOp::Eq)), Value::Num(l), Value::Num(r)) => {
-                    Ok(Value::Bool(l == r))
-                }
-                (BinOp::Rel(RelOp::Eq(EqOp::Eq)), Value::Str(l), Value::Str(r)) => {
-                    Ok(Value::Bool(l == r))
-                }
-                // Ne
-                (BinOp::Rel(RelOp::Eq(EqOp::Ne)), Value::Nil, Value::Nil) => Ok(Value::Bool(false)),
-                (BinOp::Rel(RelOp::Eq(EqOp::Ne)), Value::Nil, _)
-                | (BinOp::Rel(RelOp::Eq(EqOp::Ne)), _, Value::Nil) => Ok(Value::Bool(true)),
-                (BinOp::Rel(RelOp::Eq(EqOp::Ne)), Value::Bool(l), Value::Bool(r)) => {
-                    Ok(Value::Bool(l != r))
-                }
-                (BinOp::Rel(RelOp::Eq(EqOp::Ne)), Value::Num(l), Value::Num(r)) => {
-                    Ok(Value::Bool(l != r))
-                }
-                (BinOp::Rel(RelOp::Eq(EqOp::Ne)), Value::Str(l), Value::Str(r)) => {
-                    Ok(Value::Bool(l != r))
-                }
-                (BinOp::Rel(RelOp::Eq(_)), _, _) => Err(InvalidOperandTypeError::EqOp)?,
-                // Cmp
-                (BinOp::Rel(RelOp::Cmp(CmpOp::Lt)), Value::Num(l), Value::Num(r)) => {
-                    Ok(Value::Bool(l < r))
-                }
-                (BinOp::Rel(RelOp::Cmp(CmpOp::Le)), Value::Num(l), Value::Num(r)) => {
-                    Ok(Value::Bool(l <= r))
-                }
-                (BinOp::Rel(RelOp::Cmp(CmpOp::Gt)), Value::Num(l), Value::Num(r)) => {
-                    Ok(Value::Bool(l > r))
-                }
-                (BinOp::Rel(RelOp::Cmp(CmpOp::Ge)), Value::Num(l), Value::Num(r)) => {
-                    Ok(Value::Bool(l >= r))
-                }
-                (BinOp::Rel(RelOp::Cmp(_)), _, _) => Err(InvalidOperandTypeError::CmpOp)?,
-                // Add
-                (BinOp::Add(AddOp::Add), Value::Str(l), Value::Str(r)) => Ok(Value::Str(l + &r)),
-                (BinOp::Add(AddOp::Add), Value::Num(l), Value::Num(r)) => Ok(Value::Num(l + r)),
-                (BinOp::Add(AddOp::Add), _, _) => Err(InvalidOperandTypeError::AddOpAdd)?,
-                // Sub
-                (BinOp::Add(AddOp::Sub), Value::Num(l), Value::Num(r)) => Ok(Value::Num(l - r)),
-                (BinOp::Add(AddOp::Sub), _, _) => Err(InvalidOperandTypeError::AddOpSub)?,
-                // Sub, Mul, Div
-                (BinOp::Mul(MulOp::Mul), Value::Num(l), Value::Num(r)) => Ok(Value::Num(l * r)),
-                (BinOp::Mul(MulOp::Div), Value::Num(l), Value::Num(r)) => Ok(Value::Num(l / r)),
-                (BinOp::Mul(_), _, _) => Err(InvalidOperandTypeError::MulOp)?,
+    fn eval_bin_expr(&self, op: &BinOp, l: &Expr, r: &Expr) -> Result<Value, RuntimeError> {
+        let (l, r) = (self.eval(&l)?, self.eval(&r)?);
+        match op {
+            BinOp::Rel(RelOp::Eq(op)) => Ok(Value::Bool(match op {
+                EqOp::Eq => l == r,
+                EqOp::Ne => l != r,
+            })),
+            BinOp::Rel(RelOp::Cmp(cmp)) => match (l, r) {
+                (Value::Num(l), Value::Num(r)) => Ok(Value::Bool(match cmp {
+                    CmpOp::Lt => l < r,
+                    CmpOp::Le => l <= r,
+                    CmpOp::Gt => l > r,
+                    CmpOp::Ge => l >= r,
+                })),
+                _ => Err(InvalidOperandTypeError::CmpOp)?,
+            },
+            BinOp::Add(AddOp::Add) => match (l, r) {
+                (Value::Num(l), Value::Num(r)) => Ok(Value::Num(l + r)),
+                (Value::Str(l), Value::Str(r)) => Ok(Value::Str(l + &r)),
+                _ => Err(InvalidOperandTypeError::AddOpAdd)?,
+            },
+            BinOp::Add(AddOp::Sub) => match (l, r) {
+                (Value::Num(l), Value::Num(r)) => Ok(Value::Num(l - r)),
+                _ => Err(InvalidOperandTypeError::AddOpSub)?,
+            },
+            BinOp::Mul(mul) => match (l, r) {
+                (Value::Num(l), Value::Num(r)) => Ok(Value::Num(match mul {
+                    MulOp::Mul => l * r,
+                    MulOp::Div => l / r,
+                })),
+                _ => Err(InvalidOperandTypeError::MulOp)?,
             },
         }
     }
 
     pub fn eval(&self, expr: &Expr) -> Result<Value, RuntimeError> {
         match expr {
-            Expr::Lit(lit) => self.eval_lit(&lit),
-            Expr::Un(un) => self.eval_un_expr(&un),
-            Expr::Bin(bin) => self.eval_bin_expr(&bin),
+            Expr::Lit(lit) => Ok(match lit {
+                Lit::Nil(_) => Value::Nil,
+                Lit::Bool(x) => Value::Bool(x.0),
+                Lit::Num(x) => Value::Num(x.0),
+                Lit::Str(x) => Value::Str(x.0.clone()),
+            }),
+            Expr::Un(UnExpr { op, e }) => self.eval_un_expr(op, e),
+            Expr::Bin(BinExpr { op, l, r }) => self.eval_bin_expr(op, l, r),
         }
     }
 }
