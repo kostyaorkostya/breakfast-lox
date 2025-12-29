9128d8428e8b672ac7663891de3b6b1e32ea25cf
Kostya <happy.boat9793@fastmail.com>
2025-12-29 13:28:44 +0000

    basic test for pretty printing

--- breakfast-lox/src/ast/tests.rs
+++ breakfast-lox/src/ast/tests.rs
@@ -1,1 +1,18 @@
-mod pretty {}
+mod pretty {
+    use super::super::{AddOp, BinExpr, BinOp, Expr, Lit, NumLit, Pretty, StrLit};
+    use expect_test::expect;
+
+    #[test]
+    fn test_expression() -> anyhow::Result<()> {
+        let actual = Expr::Bin(BinExpr {
+            op: BinOp::Add(AddOp::Add),
+            l: Box::new(Expr::Lit(Lit::Str(StrLit("hello".into())))),
+            r: Box::new(Expr::Lit(Lit::Num(NumLit(3f64)))),
+        })
+        .display()
+        .to_string();
+        expect![[r#"("hello" + 3)"#]]
+        .assert_eq(&actual);
+        Ok(())
+    }
+}
